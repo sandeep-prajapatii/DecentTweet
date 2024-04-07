8557f4a1b0396b26e07e7e2239421179de7013d7
sandeep-prajapatii <sandeeprajapati18@gmail.com>
2024-04-07 12:15:21 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+      Hello World
+    </div>
+  )
+}
+
+export default App
