2e72528b443d02e938199396036e3837e91673ca
shaktiiii <dubeyshakti0827@gmail.com>
2024-04-15 03:56:14 +0530

    top users lefts sides added in explore page

--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -1,9 +1,11 @@
 import Search from "../components/Search";
+import WhoToFollow from "../components/WhoToFollow";
 
 const Explore = () => {
   return (
     <div className="px-2">
       <Search />
+      <WhoToFollow />
     </div>
   );
 };
