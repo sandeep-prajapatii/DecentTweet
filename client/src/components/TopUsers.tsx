49b60cdfac7ec847d9f142bbafd705f3308b0dfd
shaktiiii <dubeyshakti0827@gmail.com>
2024-04-15 03:53:34 +0530

    top users right side done

---
+++ client/src/components/TopUsers.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { GenerateAvatar, truncateAddress } from "../helperFunctions";
+import { UserDetailsDefaultValues, UserDetailsType } from "../utils/helper";
+import { useAccount, useReadContracts, useWriteContract } from "wagmi";
+type TopUsersType = {
+  user: UserDetailsType;
+};
+
+import {
+  DecentTweetAbi as abi,
+  DecentTweetContractAddress as address,
+} from "../contract/DecentTweetABI";
+
+const TopUsers = ({ user }: TopUsersType) => {
+  const [currentUserDetails, setCurrentUserDetails] = useState<UserDetailsType>(
+    UserDetailsDefaultValues
+  );
+
+  const { writeContract } = useWriteContract();
+  const DTAAA = {
+    address: address as `0x${string}`,
+    abi,
+  };
+  const { address: currentUserAddress } = useAccount();
+  const { data: contractData } = useReadContracts({
+    contracts: [
+      {
+        ...DTAAA,
+        functionName: "getPublicUserDetails",
+        args: [currentUserAddress],
+      },
+    ],
+  });
+
+  const handleFollowUnfollow = () => {
+    if (
+      user.userAddress &&
+      currentUserDetails.following.includes(user.userAddress as `0x${string}`)
+    ) {
+      writeContract({
+        ...DTAAA,
+        functionName: "unfollowUser",
+        args: [user.userAddress],
+      });
+    } else {
+      writeContract({
+        ...DTAAA,
+        functionName: "followUser",
+        args: [user.userAddress],
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (contractData && contractData[0].result) {
+      const usrDetails = contractData[0].result as UserDetailsType;
+      setCurrentUserDetails(usrDetails);
+    }
+  }, [contractData]);
+  return (
+    <div className="flex items-center justify-between border-2 border-neutral-700 rounded-md gap-2 p-2 ">
+      <div className="flex gap-2">
+        <div>
+          <GenerateAvatar userAddress={String(user.userAddress)} size={40} />
+        </div>
+        <div className="flex-1">
+          <p className="whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
+            {user.userName}
+          </p>
+          <p className="text-sm text-neutral-400 whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
+            {truncateAddress(String(user.userAddress))}
+          </p>
+        </div>
+      </div>
+      <button
+        onClick={() => handleFollowUnfollow()}
+        className=" bg-white font-semibold text-black px-3 p-1 rounded-lg z-10"
+      >
+        {currentUserDetails.following.includes(
+          user.userAddress as `0x${string}`
+        ) ? (
+          <>
+            <p>Unfollow</p>
+          </>
+        ) : (
+          <>
+            <p>
+              {currentUserDetails.following.includes(
+                user.userAddress as `0x${string}`
+              )
+                ? "Following"
+                : "Follow"}
+            </p>
+          </>
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default TopUsers;
--- client/src/components/WhoToFollow.tsx
+++ client/src/components/WhoToFollow.tsx
@@ -1,76 +1,47 @@
-import { GenerateAvatar } from "../helperFunctions";
-import pfp1 from "../assets/pfp/pfp1.jpeg";
-import pfp2 from "../assets/pfp/pfp2.jpeg";
+import { useEffect, useState } from "react";
+import { useReadContracts } from "wagmi";
+import {
+  DecentTweetAbi as abi,
+  DecentTweetContractAddress as address,
+} from "../contract/DecentTweetABI";
+import { UserDetailsType } from "../utils/helper";
+import TopUsers from "./TopUsers";
 
 const WhoToFollow = () => {
+  const [topUsers, setTopUsers] = useState<UserDetailsType[]>([]);
+
+  const DTAAA = {
+    address: address as `0x${string}`,
+    abi,
+  };
+
+  const { data: contractData } = useReadContracts({
+    contracts: [
+      {
+        ...DTAAA,
+        functionName: "getTopUsers",
+        args: [2],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    if (contractData && contractData[0].result) {
+      const topUsrs = contractData[0].result as UserDetailsType[];
+      setTopUsers(topUsrs);
+    }
+  }, [contractData]);
   return (
     <div className="border-2 rounded-md border-neutral-700 p-2">
       <p className="text-xl font-semibold mb-4">Who to follow</p>
       <div className="flex flex-col gap-4">
-        <div className="flex items-center justify-between border-2 border-neutral-700 rounded-md gap-2 p-2 ">
-          <div className="flex gap-2">
-            {/* <img src={pfp2} className="h-14 w-14 rounded-full" alt="profile" /> */}
-            <div>
-              <GenerateAvatar
-                userAddress={"0x2342373992d054eF318081c111cF4C87B44aBa869"}
-                size={40}
-              />
-            </div>
-            <div className="flex-1">
-              <p className="whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
-                elonmush
-              </p>
-              <p className="text-sm text-neutral-400 whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
-                0x234...869
-              </p>
-            </div>
-          </div>
-          <button className=" bg-white font-semibold text-black px-3 p-1 rounded-lg z-10">
-            Follow
-          </button>
-        </div>
-        <div className="flex items-center justify-between border-2 border-neutral-700 rounded-md gap-2 p-2 ">
-          <div className="flex gap-2">
-            <div>
-              <GenerateAvatar
-                userAddress={"0x86786773992d054eF318081c111cF4C87B44aBa869"}
-                size={40}
-              />
-            </div>
-            <div className="flex-1">
-              <p className="whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
-                divine
-              </p>
-              <p className="text-sm text-neutral-400 whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
-                0x867...869
-              </p>
-            </div>
-          </div>
-          <button className=" bg-white font-semibold text-black px-3 p-1 rounded-lg z-10">
-            Follow
-          </button>
-        </div>
-        <div className="flex items-center justify-between border-2 border-neutral-700 rounded-md gap-2 p-2 ">
-          <div className="flex gap-2">
-            <div>
-              <GenerateAvatar
-                userAddress={"0x652373992d054eF318081c111cF4C87B44aBa869"}
-                size={40}
-              />
-            </div>
-            <div className="flex-1">
-              <p className="whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
-                chutki
-              </p>
-              <p className="text-sm text-neutral-400 whitespace-nowrap text-ellipsis overflow-hidden md:max-w-[150px]">
-                0x652...869
-              </p>
-            </div>
-          </div>
-          <button className=" bg-white font-semibold text-black px-3 p-1 rounded-lg z-10">
-            Follow
-          </button>
-        </div>
+        {topUsers.map((user) => {
+          return (
+            <>
+              <TopUsers user={user} />
+            </>
+          );
+        })}
       </div>
     </div>
   );
