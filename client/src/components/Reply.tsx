f354fc92a5e487a15f6479ab65ab3d0e00e6d0bb
sandeep-prajapatii <sandeeprajapati18@gmail.com>
2024-04-15 02:25:52 +0530

    followers-following-calling

--- client/src/components/Reply.tsx
+++ client/src/components/Reply.tsx
@@ -57,45 +57,43 @@ const Reply = ({ originalTweet, replyTweet }: ReplyType) => {
   }, [data]);
 
   return (
-    <>
-      <div className="border-2 border-neutral-700  rounded-md">
-        <div className=" p-2">
+    <div className="relative">
+      <div className="flex z-50 relative">
+        <div className="m-1">
+          <GenerateAvatar
+            userAddress={originalTweetData.authorAddress}
+            size={40}
+          />
+        </div>
+        <div className="gap-2">
           <div className="flex items-center gap-2">
-            <div className="h-10 w-10 object-contain">
-              <GenerateAvatar
-                userAddress={originalTweetData.authorAddress}
-                size={40}
-              />
-            </div>
-            <div className="flex items-center gap-2">
-              {originalTweetData.authorName !== "" && (
-                <>
-                  <p>{originalTweetData.authorName}</p>
-                  <span className="h-1 w-1 bg-neutral-300 rounded-full"></span>
-                </>
-              )}
-              <p>{truncateAddress(originalTweetData.authorAddress)}</p>
-              <span className="h-1 w-1 bg-neutral-300 rounded-full"></span>
-              <p className="text-neutral-500">
-                {calculateTimeDifference(Number(originalTweetData.timestamp))}
-              </p>
-            </div>
+            {originalTweetData.authorName !== "" && (
+              <>
+                <p>{originalTweetData.authorName}</p>
+                <span className="h-1 w-1 bg-neutral-300 rounded-full"></span>
+              </>
+            )}
+            <p>{truncateAddress(originalTweetData.authorAddress)}</p>
+            <span className="h-1 w-1 bg-neutral-400 rounded-full"></span>
+            <p className="text-neutral-500">
+              {calculateTimeDifference(Number(originalTweetData.timestamp))}
+            </p>
           </div>
           <p className="mt-2">{originalTweetData.tweetMsg}</p>
         </div>
       </div>
-
-      <div className="mx-auto w-[90%]">
-        <div
-          onClick={() => navigate(`/profile/${currentTweetData.authorAddress}`)}
-          className="flex items-center gap-2 mb-2  "
-        >
-          <div className="h-10 w-10 object-contain">
-            <GenerateAvatar
-              userAddress={currentTweetData.authorAddress}
-              size={40}
-            />
-          </div>
+      <span className="absolute h-[70%]  border border-neutral-700 top-1/2 -translate-y-1/2 left-[22px] "></span>
+      <div
+        onClick={() => navigate(`/profile/${currentTweetData.authorAddress}`)}
+        className="flex z-50 mt-2  relative"
+      >
+        <div className="m-1">
+          <GenerateAvatar
+            userAddress={currentTweetData.authorAddress}
+            size={40}
+          />
+        </div>
+        <div className=" gap-2">
           <div className="flex items-center gap-2">
             {currentTweetData.authorName !== "" && (
               <>
@@ -104,20 +102,20 @@ const Reply = ({ originalTweet, replyTweet }: ReplyType) => {
               </>
             )}
             <p>{truncateAddress(currentTweetData.authorAddress || "")}</p>
-            <span className="h-1 w-1 bg-neutral-300 rounded-full"></span>
-            <p className="text-neutral-500">
+            <span className="h-1 w-1 bg-neutral-400 rounded-full"></span>
+            <p className="text-neutral-500 ">
               {calculateTimeDifference(Number(currentTweetData.timestamp))}
             </p>
           </div>
+          <p
+            onClick={() => navigate(`/tweet/${currentTweetData.tweetIndex}`)}
+            className="mt-2"
+          >
+            {currentTweetData.tweetMsg}
+          </p>
         </div>
-        <p
-          onClick={() => navigate(`/tweet/${currentTweetData.tweetIndex}`)}
-          className="mb-2"
-        >
-          {currentTweetData.tweetMsg}
-        </p>
       </div>
-    </>
+    </div>
   );
 };
 
